d0c54d57a52a6f9bee30df1b323dbe5ee4beddbf
Venicio Sachetty <sachetty.sa@gmail.com>
2021-09-28 20:05:54 -0300

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css'
+
+export default ({black}) =>{
+    return(
+        <header className= {black ? 'black' : ''}>
+            <div className= 'header--logo'>
+                <a href= '/'>
+                    <img src= 'https://logodownload.org/wp-content/uploads/2014/10/netflix-logo-5.png' alt = 'Netflix'/>
+                </a>
+            </div>
+
+            <div className= 'header--user'>
+                <a href= '/'>
+                    <img src= 'https://i.pinimg.com/originals/b6/77/cd/b677cd1cde292f261166533d6fe75872.png ' alt ='user'/>
+                </a>
+            </div>
+        </header>
+    )
+}
